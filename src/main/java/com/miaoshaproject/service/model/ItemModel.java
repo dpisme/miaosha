43975adb5d5ec13470293bf52b54437ac3b8b30f
汪大鹏 <1639489689@qq.com>
2019-01-10 22:58:46 +0800

    first commit

--- src/main/java/com/miaoshaproject/service/model/ItemModel.java
+++ src/main/java/com/miaoshaproject/service/model/ItemModel.java
@@ -1,5 +1,8 @@
 package com.miaoshaproject.service.model;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
@@ -10,21 +13,27 @@ public class ItemModel {
     private Integer id;
 
     //商品名称
+    @NotBlank(message = "商品名称不能为空")
     private String title;
 
     //商品价格
+    @NotNull(message = "商品价格不能为空")
+    @Min(value = 0,message = "商品价格必须大于0")
     private BigDecimal price;
 
     //商品的库存
+    @NotNull(message = "库存不能不填")
     private Integer stock;
 
     //商品的描述
+    @NotBlank(message = "商品描述信息不能为空")
     private String description;
 
     //商品的销量
-    private String sales;
+    private Integer sales;
 
     //商品描述图片的url
+    @NotBlank(message = "商品图片信息不能为空")
     private String imgUrl;
 
     public Integer getId() {
@@ -67,11 +76,11 @@ public class ItemModel {
         this.description = description;
     }
 
-    public String getSales() {
+    public Integer getSales() {
         return sales;
     }
 
-    public void setSales(String sales) {
+    public void setSales(Integer sales) {
         this.sales = sales;
     }
 
